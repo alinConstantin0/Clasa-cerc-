de6753c846fa2114cc82a782b766564a9fe86a0e
alinConstantin <alinghinea12@gmail.com>
2021-02-11 13:01:50 +0200

    no message

---
+++ src/com/company/Cerc.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Cerc {
+    private double raza;
+
+    public Cerc(double raza) {
+        this.raza = raza;
+    }
+
+    public double calculeazaAria() {
+        double raza = this.raza;
+        double rezultat;
+        return rezultat = Math.PI * (raza * raza);
+    }
+}
+
